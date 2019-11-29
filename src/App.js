9ca2e8cd919ed06a5f1aba7f028c7edf735833ac
Jeremy Plack <stlouisweb@gmail.com>
2019-11-29 10:48:41 -0600

    adds router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import { Router, Link } from '@reach/router';
 import logo from './logo.svg';
 import './App.css';
+
+let Home = () => <p>Edit <code>src/App.js</code> and save to reload.</p>;
+let Dashboard = () => <p>Dashboard</p>;
 
 function App() {
   return (
     <div className="App">
+      <Link to="/">Home</Link>
+      <Link to="/dash">Dashboard</Link>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <Home path="/" />
+          <Dashboard path="/dash" />
+        </Router>
         <a
           className="App-link"
           href="https://reactjs.org"
